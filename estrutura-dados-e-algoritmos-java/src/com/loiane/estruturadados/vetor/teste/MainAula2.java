80ae1a37a43ad308999f6f9d00afd84e64a7311b
Thamyris <tgthurler@gmail.com>
2022-01-25 14:39:51 -0300

    Iniciado o curso de estrutura de dados com implementações básicas de vetores

---
+++ estrutura-dados-e-algoritmos-java/src/com/loiane/estruturadados/vetor/teste/MainAula2.java
@@ -0,0 +1,15 @@
+package com.loiane.estruturadados.vetor.teste;
+
+import com.loiane.estruturadados.vetor.Vetor;
+
+public class MainAula2 {
+
+	public static void main(String[] args) {
+		Vetor vetor = new Vetor(5);
+		
+		int i = 0;
+		i++;
+
+	}
+
+}
