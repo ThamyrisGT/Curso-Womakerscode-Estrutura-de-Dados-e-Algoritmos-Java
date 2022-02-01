6466962d530732004cbdf982ed8accfd65b272f9
Thamyris <tgthurler@gmail.com>
2022-02-01 20:08:18 -0300

    Método desenfileirar implementado

--- estrutura-dados-e-algoritmos-java/src/com/loiane/estruturadados/fila/Fila.java
+++ estrutura-dados-e-algoritmos-java/src/com/loiane/estruturadados/fila/Fila.java
@@ -30,4 +30,19 @@ public class Fila<T> extends EstruturaEstatica<T> {
 		return this.elementos[0];
 	}
 
+	public T desenfileira() {    // FIFO - first in first out
+
+		final int POS = 0;
+
+		if (this.estaVazia()) {
+			return null;
+		}
+
+		T elementoASerRemovido = this.elementos[POS];
+
+		this.remove(POS);
+
+		return elementoASerRemovido;
+	}
+
 }
