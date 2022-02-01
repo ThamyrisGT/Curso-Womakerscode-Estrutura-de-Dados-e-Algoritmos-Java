3ab2a7b53cbce46c25d1553b183621e026806bd3
Thamyris <tgthurler@gmail.com>
2022-02-01 19:56:25 -0300

    Iniciando o conceito de Filas

---
+++ estrutura-dados-e-algoritmos-java/src/com/loiane/estruturadados/fila/Fila.java
@@ -0,0 +1,18 @@
+package com.loiane.estruturadados.fila;
+
+import com.loiane.estruturadados.vetor.base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+
+	public Fila() {
+		super();
+	}
+
+	public Fila(int capacidade) {
+		super(capacidade);
+	}
+
+
+
+	}
+}
